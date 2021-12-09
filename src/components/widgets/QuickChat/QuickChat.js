b6b4070476d3b2571769976267d1015e352dca8b
Draster <kangurexplay@gmail.com>
2021-12-09 12:21:43 +0100

    test

--- src/components/widgets/QuickChat/QuickChat.js
+++ src/components/widgets/QuickChat/QuickChat.js
@@ -3,19 +3,34 @@ import './QuickChat.css';
 
 class QuickChat extends React.Component{
 
+    constructor(props){
+        super(props);
+
+        this.chatStatus = this.chatStatus.bind(this);
+    }
+    componentDidMount() {
+    }
+    
     render(){
         return(
-            <div class="quick-chat__wrapper" data-hidden="true">
+            <div className="quick-chat__wrapper" data-hidden="true">
                 <div className="quick-chat__top-bar">
-                    <button onClick={this.chatStatus(this)}>＿</button>
+                    <button onClick={this.quickChatHandleClick}>＿</button>
                 </div>
                 Quick Chat
             </div>
         );
     }
-
+    
     chatStatus(){
         const chatWrapper = document.querySelector('.quick-chat__wrapper');
+
+        console.log(chatWrapper);
+        // if(chatWrapper.getAttribute('data-hidden') === 'true'){
+        //     chatWrapper.setAttribute('aria-hidden', 'false'); 
+        // }else{
+        //     chatWrapper.setAttribute('aria-hidden', 'true'); 
+        // }
     }
 
 }
